446e4a91d195b0e2c14eafd8fb4724aeb783fc75
jchristian1 <jchristian_1@hotmail.com>
2022-12-06 13:35:11 -0500

    Creating test for the user API.

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('spi/schema', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/schema', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
